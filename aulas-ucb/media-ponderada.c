58c00b183126d64574247ce75b5e2495f422611b
Daniel Gama <danielgmbr30@gmail.com>
2025-08-11 15:28:08 -0300

    exercicio media
    1 exercicio sobre media aritmetica pedida pelo professor, e outro sobre media ponderada, criei uma pasta chamada ucb para arquivos da faculdade tambem

---
+++ aulas-ucb/media-ponderada.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	float n1,n2,n3,p1,p2,p3, mediap;
+	printf("CALCULADORA DE MEDIA PONDERADA\n\n");
+	printf("Digite a primeira nota e seu respectivo peso:\n");
+	scanf("%f%f", &n1, &p1);
+	printf("Digite a segunda nota e seu respectivo peso:\n");
+	scanf("%f%f", &n2, &p2);
+	printf("Digite a terceira nota e seu respectivo peso:\n");
+	scanf("%f%f", &n3, &p3);
+	mediap = (n1 * p1 + n2 * p2 + n3 * p3) / p1 + p2 + p3;
+	printf("A media ponderada e: %.2f", mediap);
+	return 0;
+}
