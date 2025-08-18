0496a9c4ead8d2014e93741f31bfb2d7e04213e6
Daniel Gama <danielgmbr30@gmail.com>
2025-08-18 15:21:11 -0300

    Update getche_getch.c

--- capitulo2/praticando/getche_getch.c
+++ capitulo2/praticando/getche_getch.c
@@ -7,7 +7,7 @@ int main() {
 	printf("\nPressione uma tecla (getche): ");
 	ch1 = getche(); // Aguarda uma tecla no teclado 
 	printf("\nPressione outra tecla (getch): ");
-	ch2 = getch(); // Aguarda uma tecla no teclado e quando digitada nao mostra para o usuario, so quando puxada na memoria
+	ch2 = getch(); // Aguarda uma tecla no teclado e quando digitada nao mostra para o usuario
 	printf("\nA tecla sucessora ASCII e: %c", ch1 + 1);
 	printf("\nA tecla antecessora ASCII e: %c", ch2 - 1);
 	return 0;
