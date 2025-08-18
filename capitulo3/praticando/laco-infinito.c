523818dffaab9b53829c6226ea3fdf98a190d183
Daniel Gama <danielgmbr30@gmail.com>
2025-08-18 17:02:29 -0300

    so um laco infinito
    repete "Laco infinito"

---
+++ capitulo3/praticando/laco-infinito.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+//a unica coisa que esse programa faz é repetir o printf, nao execute ele, serio, ele so faz isso
+int main() {
+	for(;;)
+	printf("Laco infinito\n"); 
+	return 0;
+}
