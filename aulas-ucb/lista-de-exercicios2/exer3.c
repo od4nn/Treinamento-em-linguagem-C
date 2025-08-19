842bb5d312d7fb3d8329687e8c54611c4bd28040
Daniel Gama <danielgmbr30@gmail.com>
2025-08-19 20:23:33 -0300

    verifica qual numero é maior

--- aulas-ucb/lista-de-exercicios2/exer3.c
+++ aulas-ucb/lista-de-exercicios2/exer3.c
@@ -3,9 +3,9 @@
 
 int main() {
 	int n1,n2, m;
-	printf("Digite o primeiro numero:\n");
+	printf("Digite os numeros:\n");
 	scanf("%d%d", &n1, &n2);
-	m = (n1 > n2) ? n1:n2;
+	m = (n1 > n2) ? n1:n2; // se n1 maior que n2 for verdadeiro, m = n1, se for falso, m = n2
 	printf("%d e o maior", m);
 	return 0;
 }
