c45ba82c348da92f6c2aaa2cdfa7abae5cdc52a6
Daniel Gama <danielgmbr30@gmail.com>
2025-08-20 15:13:15 -0300

    comentando

--- aulas-ucb/lista-de-exercicios2/exer4-outraforma.c
+++ aulas-ucb/lista-de-exercicios2/exer4-outraforma.c
@@ -11,20 +11,21 @@ int main() {
     printf("Digite o terceiro numero:\n");
     scanf("%d", &n3);
     
-    if(n1 > n2){
+    if(n1 > n2){ //passa o menor valor para n1
     	temp = n1;
     	n1 = n2;
     	n2 = temp;
 	}
-	if(n1 > n3){
+	if(n1 > n3){ //passa o menor valor para n1
     	temp = n1;
     	n1 = n3;
     	n3 = temp;
 	}
-	if(n2 > n3){
+	// n2 e n3 tem agora os dois maiores valores, n1 tem o menor, entao compare n2 e n3
+	if(n2 > n3){ 
 		temp = n2;
 		n2 = n3;
-		n3 = temp;
+		n3 = temp; //passa o menor valor para n2
 	}	
 	printf("%d, %d, %d\n", n1, n2, n3);
 	return 0;
