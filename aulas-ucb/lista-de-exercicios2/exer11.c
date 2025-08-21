ac8dc9a7284f0ad8d9f4c8e303af3f87d3f52a84
Daniel Gama <danielgmbr30@gmail.com>
2025-08-21 15:28:45 -0300

    codigo que mostra quando tempo durou o jogo
    comentando ele porque ate eu me confundi nesse codigo slk

--- aulas-ucb/lista-de-exercicios2/exer11.c
+++ aulas-ucb/lista-de-exercicios2/exer11.c
@@ -16,22 +16,25 @@ int main() {
 	scanf("%d", &h2);
 	printf("Digite o minuto final no formato 24hrs: ");
 	scanf("%d", &m2);
-	totalm1 = (h1 * 60) + m1;
-	totalm2 = (h2 * 60) + m2;
-	if(totalm2 > totalm1){
-		duracao_total_m = totalm2 - totalm1;
+	totalm1 = (h1 * 60) + m1; //pega o total de minutos do inicio do jogo
+	totalm2 = (h2 * 60) + m2; //pega o total de minutos do final do jogo
+	if(totalm2 > totalm1){ //se o final for maior que o comeco entao acabou no mesmo dia
+		duracao_total_m = totalm2 - totalm1; //guarda total em minuto
 		printf("\nO jogo durou %.2d:%.2d hrs e terminou no mesmo dia.\n\n");
 	}
-	else if(totalm1 > totalm2){
-		duracao_total_m = (1440 - totalm1) + totalm2;
+	else if(totalm1 > totalm2){ // se o valor inicial e maior entao acabou no dia seguinte
+		/*1440 = 24hrs em minutos, para tirar a diferenca
+		 e calculo ser correto
+		 depois é somado, sem a diferenca de 24hrs, a totalm2*/
+		duracao_total_m = (1440 - totalm1) + totalm2; 
 		printf("\nO jogo terminou no dia seguinte.\n\n");
 	}
 	else{
-		duracao_total_m = 1440;
+		duracao_total_m = 1440; //somente caso o total1 e 2 sejam iguais
 		printf("\nO jogo durou 24:00hrs.\n\n");
 	}
-		horas_total = duracao_total_m / 60;
-		minutos_total = duracao_total_m % 60;
+		horas_total = duracao_total_m / 60; //pega horas, dividir minutos por 60
+		minutos_total = duracao_total_m % 60; //tudo que for resto da divisao sera os minutos
 		printf("O tempo de jogo foi de: %.2d:%.2dhrs\n", horas_total, minutos_total);
 	return 0;
 }
