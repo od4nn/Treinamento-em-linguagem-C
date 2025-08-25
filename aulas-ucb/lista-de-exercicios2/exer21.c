6963a8fd09304ff1695c90862b57f0685c8a07e6
Daniel Gama <danielgmbr30@gmail.com>
2025-08-25 09:31:51 -0300

    quase mesma coisa do exer20
    muda so certos valores, mesma logica

---
+++ aulas-ucb/lista-de-exercicios2/exer21.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	float peso, pesokg, valor_total, preco_em_kg ,valor_total_imp, imposto;
+	int codigo_carga, codigo_origem;
+	
+	printf("Informe o codigo da carga: ");
+	scanf("%d", &codigo_carga);
+	printf("Informe o peso do produto em toneladas: ");
+	scanf("%f", &peso);
+	printf("Informe o codigo do estado de origem: ");
+	scanf("%d", &codigo_origem);
+	pesokg = peso * 1000;
+	
+	//verificando codigo da carga
+	if(codigo_carga >= 31 && codigo_carga <= 40){ 
+		preco_em_kg = 340;	
+	}
+	else if(codigo_carga >= 21){
+		preco_em_kg = 250;
+	}
+	else{
+		preco_em_kg = 100;
+	}
+	
+	//verificando codigo de origem
+	if(codigo_origem == 1){
+		imposto = 0.35;
+	}
+	else if(codigo_origem == 2){
+		imposto = 0.25;
+	}
+	else if(codigo_origem == 3){
+		imposto = 0.15;
+	}
+	else if(codigo_origem == 4){
+		imposto = 0.05;
+	}
+	else if(codigo_origem == 5){
+		imposto = 0.0;
+	}
+	
+	valor_total = (pesokg * preco_em_kg);
+	valor_total_imp = valor_total + (valor_total * imposto);
+	
+	printf("\n\nO peso da carga em kg e: %.2f", pesokg);
+	printf("\nO preco total do produto e: RS%.2f", valor_total);
+	printf("\nO imposto foi de: RS%.2f ", (valor_total * imposto));
+	printf("\nE o valor total mais o imposto e: RS%.2f", valor_total_imp);
+	return 0;
+}
