b7d9173fa561c2f4cb142091c2792b78868de937
Daniel Gama <danielgmbr30@gmail.com>
2025-08-26 17:35:13 -0300

    mostra a tabela ascii, valor decimal, char e hex

--- capitulo3/exercicios_finais/exer3/main.c
+++ capitulo3/exercicios_finais/exer3/main.c
@@ -3,6 +3,9 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    printf("TABELA ASCII:\n\n");
+    for(int i = 32; i <= 255; i++){
+        printf("Caractere = %c\tDecimal = %d\tHexadecimal = %x\n\n", i, i, i);
+    }
     return 0;
 }
