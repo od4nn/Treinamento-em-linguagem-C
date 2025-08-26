4792e64d581276603c99693c8e743f8529177ee6
Daniel Gama <danielgmbr30@gmail.com>
2025-08-26 18:13:35 -0300

    verifica se o usuario digitou esc
    se a letra digitada for minuscula, aparece maiscula

--- capitulo3/exercicios_finais/exer4/main.c
+++ capitulo3/exercicios_finais/exer4/main.c
@@ -1,8 +1,17 @@
-#include <stdio.h>
+#include <stdio.h
 #include <stdlib.h>
+#include <conio.h>
 
 int main()
 {
-    printf("Hello world!\n");
+    do{
+        char resp;
+        printf("Digite uma tecla:");
+        resp = getch();
+        //operador ternario, se a expressao for verdadeira o primeiro valor depois de ? sera passado, senao o valor depois de :
+        resp = (resp >= 97 && resp <= 122) ? resp - 32: resp;
+        printf("\n%c\n", resp);
+    }
+    while(resp != 27); //27 igual a esc na tabela ascii
     return 0;
 }
