3ffe013441ba8bce99e9e2125e54b08d005bc17b
Daniel Gama <danielgmbr30@gmail.com>
2025-08-26 16:51:36 -0300

    programa que calcula aumento de salario com o passar dos anos
    compara o aumento de um salario que recebe promoção de 1.5% no primeiro ano, e que dobra a cada ano ate o ano atual

--- aulas-ucb/lista-de-exercicios3/exer1/main.c
+++ aulas-ucb/lista-de-exercicios3/exer1/main.c
@@ -10,14 +10,18 @@ Faça um programa que determine o salário atual desse funcionário.*/
 
 int main()
 {
+    //primeiro salario
     float salario_2005 = 1000.0;
-    float salario_2006, salario_2025, salario_atual;
-    float porcent = 0.015;
-    salario_2006 = salario_2005 * 1.015;
+    float salario_2006, salario_2025;
+    // definindo a primeira porcentagem
+    float porcent = 0.015, calcul_porcent;
+    salario_2006 = salario_2005 * 1.015; //calculando o salario de 2006 com base na porcentagem
+    float salario_atual = salario_2006; //dando um valor inicial para salario_atual
 
     for(int i = 2007; i <= 2025; i++){
-        porcent *= 2;
-        salario_atual = salario_2006 + (salario_2006 * porcent);
+        porcent *= 2; //dobrando a porcentagem a cada looping
+        calcul_porcent = salario_atual * porcent; //pega cada aumento em dinheiro de cada ano
+        salario_atual += calcul_porcent; //soma o aumento ao salario mais recente
     }
 
     printf("O salario dessa pessoa em 2025 sera de: RS %.2f\n", salario_atual);
