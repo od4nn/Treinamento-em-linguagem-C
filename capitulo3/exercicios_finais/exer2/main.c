84a08e9ddf03254aa15446be3b7132d354b783d1
Daniel Gama <danielgmbr30@gmail.com>
2025-08-26 17:22:43 -0300

    mostra um I gigante e um E gigante

---
+++ capitulo3/exercicios_finais/exer2/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   printf("IIIIIII\n");
+   for(int i = 0; i < 17; i++){
+        printf(" III\n");
+    }
+
+    printf("IIIIIII\n");
+    printf("\n\n\n");
+    system("PAUSE");
+    system("cls");
+
+    printf("IIIIIIIII\n");
+
+    for(int i = 0; i < 3; i++){
+        printf("I\n");
+    }
+
+    printf("IIIIIIIII\n");
+
+    for(int i = 0; i < 3; i++){
+        printf("I\n");
+    }
+
+    printf("IIIIIIIII\n");
+    return 0;
+}
+
