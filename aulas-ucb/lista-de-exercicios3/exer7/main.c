8eff4b01f17d547620b906d0d9eafccddf698424
Daniel Gama <danielgmbr30@gmail.com>
2025-08-28 10:29:09 -0300

    calcula raiz quadrada

---
+++ aulas-ucb/lista-de-exercicios3/exer7/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    double k = 1.0, n;
+    printf("Informe o valor de n: ");
+    scanf("%lf", &n);
+
+    for(int i = 0; i < 12; i++){
+        k = (k + (n / k)) / 2; //formula de calculo de raiz quadrada
+        printf("\nA raiz quadrada de n no loop %d e: %lf", i + 1, k);
+    }
+    printf("\n\n");
+
+    printf("Valor calculado apos 12 loops: %lf", k);
+    printf("\nValor real calculado pela math.h: %lf\n", sqrt(n));
+    return 0;
+}
