3619604422fca0a11c5e6c48184b0377f4f4f0fd
Daniel Gama <danielgmbr30@gmail.com>
2025-10-07 10:04:00 -0300

    funcao recursiva que soma todos os numeros anteriores
    funcao recursiva é mais fé de que vai funcionar do que entender cada minima parte

--- capitulo5/exercicios-finais/ex48/main.c
+++ capitulo5/exercicios-finais/ex48/main.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int soma(int n);
 int main()
 {
-    printf("Hello world!\n");
+    int n, total;
+    printf("Informe um numero: ");
+    scanf("%d", &n);
+    total = soma(n);
+    printf("%d", total);
     return 0;
+}
+
+int soma(int n)
+{
+    int total;
+    if(n == 1){
+        return 1;
+    }
+    total = n + soma(n - 1);
+    return total;
 }
