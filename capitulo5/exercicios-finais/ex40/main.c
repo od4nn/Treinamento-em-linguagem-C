ea58e518bcc717bb2751081a98cd02cf23f1be33
Daniel Gama <danielgmbr30@gmail.com>
2025-10-07 09:25:18 -0300

    verifica ano bissexto

--- capitulo5/exercicios-finais/ex40/main.c
+++ capitulo5/exercicios-finais/ex40/main.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int verifica_bis(int ano);
 int main()
 {
-    printf("Hello world!\n");
+    int ano, resul;
+    printf("Informe um ano: ");
+    scanf("%d", &ano);
+    resul = verifica_bis(ano);
+    printf("%d", resul);
     return 0;
+}
+
+int verifica_bis(int ano)
+{
+    if((ano % 4 == 0 && ano % 100 != 0) || ano % 400 == 0){
+        return 1;
+    }
+    else{
+        return 0;
+    }
 }
