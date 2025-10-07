8353b2f5ca5b1509d60140efa0fca2037c01f8e4
Daniel Gama <danielgmbr30@gmail.com>
2025-10-07 09:42:55 -0300

    funcao recursiva calcula que potencia

--- capitulo5/exercicios-finais/ex47/main.c
+++ capitulo5/exercicios-finais/ex47/main.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int potencia(int i, int j);
 int main()
 {
-    printf("Hello world!\n");
+    int base, exp, resul;
+    printf("Informe o numero base: ");
+    scanf("%d", &base);
+    printf("Informe o expoente: ");
+    scanf("%d", &exp);
+    resul = potencia(base, exp);
+    printf("\n\n%d elevado a %d eh: %d\n", base, exp, resul);
     return 0;
+}
+
+int potencia(int i, int j)
+{
+    int res;
+    if(j == 0){
+        return 1;
+    }
+    res = i * potencia(i, j - 1);
+    return res;
 }
