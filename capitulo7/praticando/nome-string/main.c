fa2548d100f0d62c24f3d3eec47d6abb667a1daa
Daniel Gama <danielgmbr30@gmail.com>
2025-10-08 10:05:55 -0300

    testando strings

---
+++ capitulo7/praticando/nome-string/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char nome[80];
+
+    printf("Informe seu nome: ");
+    scanf("%s", nome);
+    printf("Saudacoes %s!", nome);
+    return 0;
+}
