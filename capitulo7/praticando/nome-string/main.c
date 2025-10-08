c78c2266f33354ea27853f92b45e0a007053858c
Daniel Gama <danielgmbr30@gmail.com>
2025-10-08 10:24:24 -0300

    atualizando, usando fgets ao inves de gets
    gemini disse que é mais segura

--- capitulo7/praticando/nome-string/main.c
+++ capitulo7/praticando/nome-string/main.c
@@ -6,7 +6,13 @@ int main()
     char nome[80];
 
     printf("Informe seu nome: ");
-    scanf("%s", nome);
+    fgets(nome, 80, stdin); //alternativa segura para gets
+
+    /*Caso queira deixar a quebra de linha corretamente use:
+    nome[strcspn(nome, "\n")] = '\0';
+    antes do printf, substitue o \n por \0
+    caso contrario o ! vai aparecer um linha embaixo de nome*/
+
     printf("Saudacoes %s!", nome);
     return 0;
 }
