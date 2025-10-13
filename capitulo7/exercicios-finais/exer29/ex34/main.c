ec628678e3f73dcc039ce786939d844ee019e8c7
Daniel Gama <danielgmbr30@gmail.com>
2025-10-13 10:45:40 -0300

    inverte uma palavra

--- capitulo7/exercicios-finais/exer29/ex34/main.c
+++ capitulo7/exercicios-finais/exer29/ex34/main.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+void inverte(char palavra[], int tamanho);
 
 int main()
 {
-    printf("Hello world!\n");
+    char palavra[15];
+    int tamanho;
+
+    printf("Digite uma palavra: ");
+    scanf("%s", palavra);
+    tamanho = strlen(palavra);
+    inverte(palavra, tamanho);
     return 0;
+}
+
+void inverte(char palavra[], int tamanho)
+{
+    int i;
+    //i vai da ultima letra ate a primeira [0]
+    for(i = tamanho - 1; i >= 0; i--){ // -1 para que palavra[i] acesse somente as letras da palavra
+        printf("%c", palavra[i]);
+    }
 }
