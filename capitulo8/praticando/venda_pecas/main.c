39bb850aba0dd18bacced19deb7cfad820b00c43
Daniel Gama <danielgmbr30@gmail.com>
2025-10-23 20:47:49 -0300

    fazendo soma com structs

---
+++ capitulo8/praticando/venda_pecas/main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+int main() {
+    typedef struct {
+        int pecas;
+        float preco;
+    }Venda;
+
+    Venda a = {20, 75.6}, b = {22, 55.0}, total;
+    total.pecas = a.pecas + b.pecas;
+    total.preco = a.preco + b.preco;
+    printf("%d\n%f", total.pecas, total.preco);
+}
