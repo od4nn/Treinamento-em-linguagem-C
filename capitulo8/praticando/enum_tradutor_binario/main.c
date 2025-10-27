7d72e275a2b94b1ea8252ac60eb8ffa6be371200
Daniel Gama <danielgmbr30@gmail.com>
2025-10-27 11:39:23 -0300

    criando um tradutor de binario

--- capitulo8/praticando/enum_tradutor_binario/main.c
+++ capitulo8/praticando/enum_tradutor_binario/main.c
@@ -1,29 +1,38 @@
 #include <stdio.h>
+#include <string.h>
 
-#define TAMANHO 30
+#define TAMANHO 50
 
 typedef struct {
-    unsigned char bit0: 0;
+    unsigned char bit0: 1;
     unsigned char bit1: 1;
-    unsigned char bit3: 2;
-    unsigned char bit4: 3;
-    unsigned char bit5: 4;
-    unsigned char bit6: 5;
-    unsigned char bit7: 7;
-    unsigned char bit2: 7;
+    unsigned char bit2: 1;
+    unsigned char bit3: 1;
+    unsigned char bit4: 1;
+    unsigned char bit5: 1;
+    unsigned char bit6: 1;
+    unsigned char bit7: 1;
 }Bits;
 
-typedef union {
-    unsigned char ch;
-    Bits b;
+typedef union { //divide o mesmo espaço para as duas variaveis
+    unsigned char ch; //1 byte
+    Bits b; //fatia o 1 byte em 8 bits
 }Conversor;
 
-void mostra_binario (Conversor B);
+void mostra_binario (unsigned char caractere);
 
 int main() {
+    int i;
     char palavra[TAMANHO];
-    printf("Digite uma palavra de ate 14 letras: ");
+    printf("Digite uma pequena frase de ate 50 caracteres letras: ");
     fgets(palavra, TAMANHO, stdin);
+    palavra[strcspn(palavra, "\n")] = '\0'; //substitui o \n que fgets guarda pelo terminador nulo
+
+    printf("\nConvertendo '%s' para binario: \n\n", palavra);
+
+    for (i = 0; palavra[i] != '\0'; i++) {
+        mostra_binario(palavra[i]); //manda um caractere para ser convertido em binario, faz isso ate pegar todos os caracteres da frase
+    }
     return 0;
 }
 
@@ -38,5 +47,5 @@ void mostra_binario (unsigned char caractere) {
     printf("%d", lupa.b.bit3);
     printf("%d", lupa.b.bit2);
     printf("%d", lupa.b.bit1);
-    printf("%d", lupa.b.bit0);
+    printf("%d ", lupa.b.bit0);
 }
